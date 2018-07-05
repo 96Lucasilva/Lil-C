1f28d9c60c209f44657b0ae5288e1e7f2991c9e7
Lucas Silva Oliveira <40829823+96Lucasilva@users.noreply.github.com>
2018-07-05 16:04:48 -0300

    Update List 1.c

--- List 1.c
+++ List 1.c
@@ -3,6 +3,11 @@
 
 //=========================================
 //============== LIST 1 ===================
+#include <stdio.h>
+#define ex6
+
+//=========================================
+//============== LIST 1 ===================
 //=========================================
 
 #ifdef ex1
@@ -32,6 +37,7 @@ if(n1>n2){
 printf("\n1st number: %i", n1);
 printf("\n2nd number: %i", n2);
 printf("\nThe difference between them is: %i\n", res);
+printf("\n==============================================\n");
 
 printf("\nRepeat? Y for Yes, any for No\n");
     scanf(" %c", &op);
@@ -101,3 +107,119 @@ printf("Thanks!\n\n");
 system("pause");
 }
 #endif // ex2
+
+/*==================================================================*/
+
+#ifdef ex3
+
+/* Enter a number, print him and his module. A positive number's
+   module is himself and a negative number's module is the same
+   number, but positive. */
+
+int main(){
+int n1;
+char op;
+
+do{
+printf("|| EX. 3 ||\n");
+printf("===========\n\n");
+
+printf("Enter a number:\n");
+    scanf("%i", &n1);
+printf("\nThe entered number is: %i and Its module is ", n1);
+    if(n1<0){
+        printf("%i\n", n1*-1);
+    } else {
+        printf("%i\n", n1);
+        }
+printf("\n==============================================\n");
+printf("\nRepeat? Y for Yes, any for No\n");
+    scanf(" %c", &op);
+system("cls");
+} while (op=='Y' || op=='y');
+printf("\nThanks!\n");
+
+system("pause");
+}
+#endif // ex3
+
+/*==================================================================*/
+
+#ifdef ex4
+
+/* Salary. Bonus = 5%, Tax = -7% and print the total to receive. */
+
+int main(){
+float s1, bonus, fee, s2;
+char op;
+
+do{
+printf("|| EX. 4 ||\n");
+printf("===========\n\n");
+printf("Enter the salary:\n$ ");
+    scanf("%f", &s1);
+
+bonus = s1*0.05;
+fee = (s1*0.07)*-1;
+s2 = bonus+fee;
+
+
+printf("\nBonus (+5%% of $ %.2f) = R$ %.2f\n", s1, bonus);
+printf("Tax (-7%% of $ %.2f) = R$ %.2f\n", s1, fee);
+printf("\nTotal to receive: $ %.2f", s1+s2);
+printf("\n==========================================\n");
+printf("\nRepeat? Y for Yes, any for No\n");
+    scanf(" %c", &op);
+system("cls");
+} while (op=='Y' || op=='y');
+printf("\nThanks!\n");
+
+system("pause");
+
+}
+
+#endif // ex4
+
+/*==================================================================*/
+
+#ifdef ex5
+
+
+
+#endif // ex5
+
+/*==================================================================*/
+
+#ifdef ex6
+int main (){
+int n1,n2,res;
+char op;
+
+do{
+printf("Enter the first number.\n");
+    scanf("%i", &n1);
+printf("Enter the second number.\n");
+    scanf("%i", &n2);
+
+res=n1+n2;
+printf("\n%i + %i = %i\n",n1,n2,res);
+    if(res>=10){
+        printf("Bigger or equal to 10 !\n%i + 5 = %i", res, res+5);
+    } else {
+        printf("Lower than 10.\n%i - 7 = %i", res, res-7);
+    }
+
+printf("\n===================================\n");
+printf("\nRepeat? Y for Yes, any for No\n");
+    scanf(" %c", &op);
+system("cls");
+} while (op=='Y' || op=='y');
+printf("\nThanks!\n");
+
+system("pause");
+
+}
+
+#endif // ex6
+
+/*==================================================================*/

a3747dc785ac7ede920d14353d8a0e27dee84e8c
Lucas Silva Oliveira <40829823+96Lucasilva@users.noreply.github.com>
2018-07-10 10:59:26 -0300

    Add files via upload

---
+++ repeat.c
@@ -0,0 +1,14 @@
+do{
+
+
+
+
+printf("\n==============================================\n");
+printf("\nRepeat? Y for Yes, any for No\n");
+    scanf(" %c", &op);
+system("cls");
+} while (op=='Y' || op=='y');
+printf("\nThanks!\n");
+
+system("pause");
+

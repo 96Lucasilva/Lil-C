cde79fcde7bc71ed7116baf217b07316f1c9f2cd
Lucas Silva Oliveira <40829823+96Lucasilva@users.noreply.github.com>
2018-07-13 13:18:21 -0300

    Update List 2.c

--- List 2.c
+++ List 2.c
@@ -17,7 +17,7 @@ printf("===========\n\n");
 printf("How many cicles? ");
     scanf("%d", &n1);
 if(n1<=0){
-    printf("Nothing!\n");
+    printf("\nNothing!\n");
     goto repeat;
 }
 printf("\n==============================================\n");
@@ -57,3 +57,148 @@ printf("Thanks!\n\n");
 system("pause");
 }
 #endif // ex1
+
+/*==================================================================*/
+
+#ifdef ex2
+int main(){
+int k, cont, n1;
+char op;
+do{
+int pos=0
+printf("|| EX. 2 ||\n");
+printf("===========\n\n");
+printf("How many numbers?\n");
+    scanf("%i", &k);
+printf("\n");
+for (cont=0; cont<k; cont++){
+
+    printf("Number %i: ", cont+1);
+        scanf("%i", &n1);
+            if(n1>=0){
+               pos=pos+1;
+            }
+}
+printf("\n==============================================\n");
+printf("You entered %i positive numbers.", pos);
+
+printf("\n==============================================\n");
+printf("\nRepeat? Y for Yes, any for No\n");
+    scanf(" %c", &op);
+system("cls");
+} while (op=='Y' || op=='y');
+printf("Thanks!\n\n");
+
+system("pause");
+
+}
+#endif // ex2
+
+/*==================================================================*/
+
+#ifdef ex3
+int main(){
+int n1, cont, age;
+
+char op;
+do{
+int total=0;
+printf("|| EX. 3 ||\n");
+printf("===========\n\n");
+printf("How many ages? ");
+    scanf("%i", &n1);
+printf("\n");
+for(cont=0; cont<n1; cont++){
+    printf("%i age: ", cont+1);
+        scanf("%i", &age);
+        printf("%i\n", age);
+        printf("%i\n", total);
+        total=total+age;
+        printf("%i\n", total);
+}
+printf("\n==============================================\n");
+printf("The sum of all ages is: %i", total);
+
+printf("\n==============================================\n");
+printf("\nRepeat? Y for Yes, any for No\n");
+    scanf(" %c", &op);
+system("cls");
+} while (op=='Y' || op=='y');
+printf("Thanks!\n\n");
+
+system("pause");
+
+}
+#endif // ex3
+
+/*==================================================================*/
+
+#ifdef ex4
+int main(){
+int k, cont, n1;
+
+char op;
+do{
+int neg=0, zero=0;
+printf("|| EX. 4 ||\n");
+printf("===========\n\n");
+printf("How many numbers? ");
+    scanf("%i", &k);
+printf("\n");
+for(cont=0; cont<k; cont++){
+    printf("Number %i: ", cont+1);
+        scanf("%i", &n1);
+            if(n1%2==0 && n1<0){
+                neg=neg+1;
+            } else if (n1==0){
+                    zero=zero+1;
+                    }
+}
+printf("\n=========================================================\n");
+printf("You've entered %i negative pair numbers and %i zeros.", neg, zero);
+
+
+
+printf("\n=========================================================\n");
+printf("\nRepeat? Y for Yes, any for No\n");
+    scanf(" %c", &op);
+system("cls");
+} while (op=='Y' || op=='y');
+printf("Thanks!\n\n");
+
+system("pause");
+}
+
+#endif // ex4
+
+/*==================================================================*/
+
+#ifdef ex5
+int main(){
+int cont, n;
+float k;
+
+char op;
+do{
+float weight=0;
+
+printf("How many weights? ");
+    scanf("%i", &n);
+for(cont=0; cont<n; cont++)
+
+
+
+printf("\n==============================================\n");
+printf("\nRepeat? Y for Yes, any for No\n");
+    scanf(" %c", &op);
+system("cls");
+} while (op=='Y' || op=='y');
+printf("Thanks!\n\n");
+
+system("pause");
+
+}
+
+
+#endif // ex5
+
